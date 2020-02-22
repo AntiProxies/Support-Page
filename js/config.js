f2a0dc3d7269d21abd3e1c63356112f95654376b
Gabriel Kanev <contact@mrgkanev.eu>
2020-02-22 12:16:40 +0200

    Updating the API key to only a specific service

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // array of Monitor-specific API keys or Main API key to list all monitors
 var __apiKeys = [
-	'ur841298-7df58af5ae025d0aa8bf3cfb', // Website
+	'm784377224-d30ef88e83c3b78655acf92f', // Website
 	'm776858319-7c829d2c3e250f85776f588e', // Not our server, just for testing sorry.
 ];
 
