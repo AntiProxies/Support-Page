a241e2ffbb1f4572d91d590b43d457d0820402d9
mrgkanev <contact@mrgkanev.eu>
2020-04-06 21:16:13 +0300

    Adding API server

--- js/config.js
+++ js/config.js
@@ -1,6 +1,7 @@
 // array of Monitor-specific API keys or Main API key to list all monitors
 var __apiKeys = [
 	'm784377224-d30ef88e83c3b78655acf92f', // Website
+	'm784690229-42146d322f9ade3798843a06', // API 
 ];
 
 // refresh interval (in seconds)
